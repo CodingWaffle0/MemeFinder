266b36076ae3554da888576c98b27fb864627cd5
Will <wegabel27@gmail.com>
2023-10-07 21:23:26 -0400

    got flask running

--- Get Memes/main.py
+++ Get Memes/main.py
@@ -1,26 +1,30 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import requests
+from bs4 import BeautifulSoup
 
-# Replace 'url_to_scrape' with the URL of the webpage you want to scrape
-url_to_scrape = 'https://9gag.com/search?query=words'
+def get_memes(word):
+	# Replace 'url_to_scrape' with the URL you want to scrape images from
+	url_to_scrape = f'https://www.google.com/search?q={word}+memes&tbm=isch'
 
-# Replace 'path_to_webdriver' with the path to your WebDriver executable
-# Example for Chrome:
-path_to_webdriver = '/path/to/chromedriver'
+	# Send an HTTP GET request to the URL
+	response = requests.get(url_to_scrape)
 
-# Create a WebDriver instance (e.g., for Chrome)
-driver = webdriver.Chrome(executable_path=path_to_webdriver)
+	# Check if the request was successful (status code 200)
+	if response.status_code == 200:
+		# Parse the HTML content of the page using BeautifulSoup
+		soup = BeautifulSoup(response.text, 'html.parser')
 
-# Navigate to the webpage
-driver.get(url_to_scrape)
+		# Find all img tags in the parsed HTML
+		img_tags = soup.find_all('img')
 
-# Find all <source> elements with type="video/mp4"
-video_sources = driver.find_elements(By.CSS_SELECTOR, 'source[type="video/mp4"]')
+		# Extract the 'src' attribute from each img tag
+		img_urls = [img['src'] for img in img_tags if 'src' in img.attrs]
 
-# Extract and print the URLs
-for source in video_sources:
-    video_url = source.get_attribute('src')
-    print(f"Video URL: {video_url}")
+		# Print the list of image URLs
+		for img_url in img_urls:
+			print(img_url)
+	else:
+		print(f"Failed to retrieve the page. Status code: {response.status_code}")
 
-# Close the WebDriver
-driver.quit()
+
+get_memes("computer science")
+get_memes("cats")
